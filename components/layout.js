d3698afa225e7ccea3a49416be5da944ba35cb22
devramsean0 <outramsean0@gmail.com>
2022-06-26 16:18:50 +0000

    LAYOUT | pain

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,11 @@
+import Image from 'next/image'
 import Metadata from './metadata'
 export default function Layout({title, children}) {
     return (
         <>
         <Metadata />
         <main>
-            <div className="page-wrapper with-navbar dark-mode">
+            <div className="page-wrapper with-navbar dark-mode with-navbar-fixed-bottom">
                 <div className='navbar justify-content-center'>
                         <h1 className='title navbar-brand'>
                             {title} | Blahaj Checker UK
@@ -12,6 +13,13 @@ export default function Layout({title, children}) {
                 </div>
                 <div className="content-wrapper">
                     {children}
+                    <p className='card justify-content-center flex'>
+                        &copy; {new Date().getFullYear()} <a href="https://sean-outram.dev">Sean Outram</a>
+                        <br />
+                            Powered by:
+                            <br />
+                            <Image src="/vercel.svg" alt="Vercel Logo" width="150px" height="100px" />
+                    </p>
                 </div>
             </div>
         </main>
